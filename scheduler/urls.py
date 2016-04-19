72f32c88e322938a595f9c4bdec223f86f84ada1
Jake Walsh <jake_walsh@yahoo.com>
2016-04-19 16:00:39 +0100

    feat: added basic functional views

--- scheduler/urls.py
+++ scheduler/urls.py
@@ -4,4 +4,5 @@ from django.conf.urls import url
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^(?P<event_id>\d+)/$', views.event, name='event'),
 ]
--- scheduler/views.py
+++ scheduler/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from scheduler.models import Event, SubEvent
 
 # Create your views here.
 
-def index(response):
-    return HttpResponse("This is the index page.")
+def index(request):
+    latest_event_list = Event.objects.order_by('-event_date')[:5]
+    context = {'latest_event_list': latest_event_list}
+    return render(request, 'scheduler/index.html', context)
+
+def event(request, event_id):
+    event = Event.objects.get(pk=event_id)
+    subevent_list = SubEvent.objects.filter(event_id=event_id).order_by('time_start')
+    context = {'subevent_list': subevent_list, 'event': event}
+    return render(request, 'scheduler/event.html', context)
