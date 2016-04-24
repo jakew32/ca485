099beddf649c4b68940042413bf4f5dcd6c3fbec
Jake Walsh <jake_walsh@yahoo.com>
2016-04-24 21:49:21 +0100

    feat: merged in changed from Anais

--- scheduler/views.py
+++ scheduler/views.py
@@ -1,18 +1,22 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from scheduler.models import Event, SubEvent
+from django.utils.safestring import mark_safe
+from scheduler.calendar import EventCalendar
+import datetime
 
 # Create your views here.
 
 def index(request):
     latest_event_list = Event.objects.order_by('event_date')
-    context = {'latest_event_list': latest_event_list}
+    cal = EventCalendar(latest_event_list).formatmonth(2016, 4)
+    context = {'latest_event_list': latest_event_list,'calendar': mark_safe(cal)}
     return render(request, 'scheduler/index.html', context)
 
 def event(request, event_id):
     event = Event.objects.get(pk=event_id)
     subevent_list = SubEvent.objects.filter(event_id=event_id).order_by('time_start')
-    context = {'subevent_list': subevent_list, 'event': event}
+    context = {'subevent_list': subevent_list, 'event': event, 'range' : range(24)}
     return render(request, 'scheduler/event.html', context)
 
 def assign(request):
